e69a419e7a82f523fae1320ca457c94b01607322
jafidelis <jafidelis@gmail.com>
2021-03-03 21:20:13 -0300

    Added contact list

---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: ListView(
+        children: [
+          Card(
+            child: ListTile(
+              title: Text(
+                'Alexandre',
+                style: TextStyle(fontSize: 24),
+              ),
+              subtitle: Text(
+                '1000',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
